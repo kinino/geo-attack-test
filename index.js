128febe3c9c8b71b27aba1db5ce9fd4f86d7f38a
Mario Sergio Fuentes <msf1013@gmail.com>
2016-09-15 18:33:21 -0500

    Render manifest.json

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/joystick', function(request, response) {
 });
 
 app.get('/manifest.json', function(request, response) {
-  response.route('pages/manifest.json');
+  response.render('pages/manifest.json');
 });
 
 app.get('/ground', function(request, response) {
